901469eb16ce9f44c8182eeedcc1bf2d813e2b8f
TeddyH08 <teddy.hemonet@gmail.com>
2022-03-17 13:11:00 +0100

    up

--- db/connectdb.php
+++ db/connectdb.php
@@ -2,7 +2,7 @@
 
 try
 {
-	$dbh = new PDO('mysql:host=localhost;dbname=tcconstruct;charset=utf8', 'root', '1234',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+	$dbh = new PDO('mysql:host=localhost;dbname=tcconstruct;charset=utf8', 'root', '',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 }
 catch (Exception $e)
 {
